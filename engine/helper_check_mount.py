44a3c52dcc0bc6f8aa744bdfbd21d1eff60f6996
Ling Ling <lingling@winhnin.local>
2012-12-20 12:48:19 +0800

    for testing

--- engine/helper_check_mount.py
+++ engine/helper_check_mount.py
@@ -6,7 +6,6 @@ def check_mount():
 	server_ip = helper_ip.get_current_server_ip()
 	directory = '/mnt/%s'%server_ip
 	if os.path.exists(directory):
-		print 'file exist \n'
 		if locate('.log', directory):
 			return True
 		else:
@@ -15,17 +14,13 @@ def check_mount():
 		return mount(server_ip, True)
 
 def locate(pattern, dir):
-	print 'locate file \n'
 	text_files = [f for f in os.listdir(dir) if f.endswith(pattern)]
-	print text_files
 	if text_files:
-		print 'True\n'
 		return True
-	print 'False\n'
 	return False
 
 def mount(server_ip, mkdir):
-	print 'mount\n'
+	directory = '/mnt/%s'%server_ip
 	try:
 		if(mkdir):
 			if not os.path.exists('/mnt'): 
@@ -33,9 +28,10 @@ def mount(server_ip, mkdir):
 			os.system('sudo mkdir /mnt/%s sudo mount -t cifs //%s/MyShare /mnt/%s/ -o username=smbuser,passwd=smbpassword'%(server_ip, server_ip, server_ip))
 		else:
 			os.system('sudo mount -t cifs //%s/MyShare /mnt/%s/ -o username=smbuser,passwd=smbpassword'%(server_ip, server_ip))
+		if not locate('.log', directory):
+			return False
 		return True
 	except Exception as exception:
-		print exception
 		return False
 
 if __name__=='__main__':
--- logic/test_daily_active_user.py
+++ logic/test_daily_active_user.py
@@ -1,4 +1,5 @@
 import os, sys
+from engine import helper_check_mount
 
 ENGINE_ROOT = os.path.join(os.path.dirname(__file__),'../engine')
 sys.path.insert(0, os.path.join(ENGINE_ROOT, "."))
@@ -12,40 +13,37 @@ import config
 
 @task
 def stat_im():
-    print "Start CRONTTAB!!!!!!!!!!!!!!!!!"
     oem_name='test'
     stat_category='test'
+    if helper_check_mount.check_mount():
+      stat_plan=Stat_plan()
+      # Dec 16 15:17:39 jason sshd[26803]: Received disconnect from 114.141.181.99: 11: Bye Bye
 
-    stat_plan=Stat_plan()
+      # NEW 
+      # 2012-12-20T10:33:09+08:00, 49.128.46.50, COOKIES=-, "GET / HTTP/1.1" 404 1066 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.14 (KHTML, like Gecko) Version/6.0.1 Safari/536.26.14"
+      
+      stat_sql_test=Stat_sql(oem_name=oem_name,stat_category=stat_category, \
+                                     select_count_distinct_collection={'uid':r'\[(\d+)\]'}, \
+                                     where={'live_log':r'(.)'}, \
+                                     group_by={'daily':r'(\d{4}\-\d{2}\-\d{2})'})
 
-    # Dec 16 15:17:39 jason sshd[26803]: Received disconnect from 114.141.181.99: 11: Bye Bye
+      #stat_sql_test=Stat_sql(oem_name=oem_name,stat_category=stat_category, \
+      #                               select_count_distinct_collection={'action_id':r'\[(\d+)\]'}, \
+      #                               where={'sshd':r'(\bsshd\b)'}, \
+      #                               group_by={'daily':r'(\d{4}\-\d{2}\-\d{2})','by_im':r'type=(\w+)'})
 
-    stat_sql_test=Stat_sql(oem_name=oem_name,stat_category=stat_category, \
-                                   select_count_distinct_collection={'action_id':r'\[(\d+)\]'}, \
-                                   where={'sshd':r'(\bsshd\b)'}, \
-                                   group_by={'daily':r'(\d{4}\-\d{2}\-\d{2})','by_im':r'type=(\w+)'})
+      stat_plan.add_stat_sql(stat_sql_test)
+      stat_plan.add_log_source(r'/var/log/secure.log')
+      stat_plan.run()    
 
-    stat_plan.add_stat_sql(stat_sql_test)
+  '''if __name__=='__main__':
 
-    stat_plan.add_log_source(r'/var/log/secure.log')
-    
-    """
-    stat_plan.add_log_source(r'\\192.168.0.135\logs_moim\morange.log.' \
-           +datetime.fromtimestamp(my_date).strftime('%Y-%m-%d') \
-           +'*')
-    stat_plan.add_log_source(r'\\192.168.0.137\logs_moim\morange.log.' \
-           +datetime.fromtimestamp(my_date).strftime('%Y-%m-%d') \
-           +'*')
-    """
-
-    stat_plan.run()    
-
-'''if __name__=='__main__':
-
-    for i in range(config.day_to_update_stat,0,-1):
-        my_date=time.time()-3600*24*i
-        stat_im(my_date)
-'''
+      for i in range(config.day_to_update_stat,0,-1):
+          my_date=time.time()-3600*24*i
+          stat_im(my_date)
+  '''
+else:
+    'MOUNT ERROR'
 
 
 
